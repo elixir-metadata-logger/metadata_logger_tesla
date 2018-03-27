755d3c8306334767a3eac3c07e2a493d5ab5be66
Chulki Lee <chulki.lee@gmail.com>
2018-03-27 10:57:59 -0700

    initial implementation

--- lib/tesla_metadata_logger.ex
+++ lib/tesla_metadata_logger.ex
@@ -1,18 +1,65 @@
 defmodule TeslaMetadataLogger do
-  @moduledoc """
-  Documentation for TeslaMetadataLogger.
-  """
+  @behaviour Tesla.Middleware
 
-  @doc """
-  Hello world.
+  require Logger
 
-  ## Examples
+  def call(env, next, _opts) do
+    req_metadata = [
+      http_client_method: env.method |> to_string() |> String.upcase(),
+      http_client_url: env.url
+    ]
 
-      iex> TeslaMetadataLogger.hello
-      :world
+    req_debug_metadata = [
+      http_client_req_headers: normalize_headers(env.headers),
+      http_client_req_body: normalize_body(env.body)
+    ]
 
-  """
-  def hello do
-    :world
+    Logger.debug("started", req_metadata ++ req_debug_metadata)
+
+    start = System.monotonic_time()
+    env = Tesla.run(env, next)
+    stop = System.monotonic_time()
+
+    resp_metadata = [
+      http_client_duration_ms: System.convert_time_unit(stop - start, :native, :millisecond),
+      http_client_status: env.status
+    ]
+
+    resp_debug_metadata = [
+      http_client_resp_headers: normalize_headers(env.headers),
+      http_client_resp_body: normalize_body(env.body)
+    ]
+
+    resp_info_metadata = [
+      http_client_resp_body_bytes: body_size(env.body)
+    ]
+
+    Logger.debug("completed", resp_metadata ++ resp_debug_metadata)
+
+    Logger.info("completd", req_metadata ++ resp_metadata ++ resp_info_metadata)
+
+    env
+  rescue
+    ex in Tesla.Error ->
+      stacktrace = System.stacktrace()
+      log_exception(ex)
+      reraise ex, stacktrace
+  end
+
+  defp normalize_headers(%{} = headers),
+    do: headers |> Enum.map(fn {k, v} -> {k, [v]} end) |> Enum.into(%{})
+
+  defp normalize_headers(headers) when is_list(headers),
+    do: headers |> Enum.group_by(fn {k, _v} -> k end, fn {_k, v} -> v end)
+
+  defp normalize_headers(headers), do: inspect(headers)
+
+  defp normalize_body(body), do: to_string(body)
+
+  defp body_size(nil), do: 0
+  defp body_size(body), do: IO.iodata_length(body)
+
+  defp log_exception(%Tesla.Error{message: message, reason: reason}) do
+    Logger.error(message, reason: inspect(reason))
   end
 end
